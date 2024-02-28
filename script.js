62347d8675381512928ccc0c0b41856465335524
RISHAV YADAV <128891361+rishavy@users.noreply.github.com>
2024-02-28 22:32:22 +0530

    DOM project 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const targetDiv = document.getElementById("target");
+
+targetDiv.style.backgroundColor = "yellow";
+targetDiv.style.margin = "20px";
+targetDiv.style.padding = "10px";
+targetDiv.style.fontSize = "18px";
+targetDiv.style.fontWeight = "bold";
+targetDiv.style.height = "200px";
+targetDiv.style.width = "300px";
+targetDiv.style.color = "blue";
